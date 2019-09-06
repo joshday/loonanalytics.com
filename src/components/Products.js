ef13e997b8f58e500d0823888fcc7fa805fba999
Josh Day <emailjoshday@gmail.com>
2019-09-06 10:08:39 -0400

    wip

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,11 +3,13 @@ import { Row, Col, Card, Container } from 'react-materialize'
 
 export default function Products() {
   return <Container>
-    <Row className="flex">
+    <Row className="flex" style={{marginTop: 20}}>
 
       <Col s={12} m={6}>
-        <Card title="ML Monitor" className="center indigo darken-4 white-text">
+        <Card title="ML Monitor" className="center cyan darken-4 white-text">
           <h4><a className="btn" target="_blank" rel="noopener noreferrer" href="http://signals.loonanalytics.com">Link</a></h4>
+          <a href="https://www.producthunt.com/posts/loon-analytics?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-loon-analytics" rel="noopener noreferrer" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=163769&theme=light" alt="Loon Analytics - Easy long-term trend monitoring | Product Hunt Embed" style={{width: 250, height: 54}} /></a>
+          <br /><br />
           <p>Monitoring API/Dashboard for machine learning models in production.</p>
           <br />
           <img src="https://user-images.githubusercontent.com/8075494/62552461-b1f95700-b83b-11e9-9ff1-9dc2b8c587a8.png"
@@ -16,8 +18,10 @@ export default function Products() {
       </Col>
 
       <Col s={12} m={6}>
-        <Card title="One Click Tuner" className="center indigo darken-4 white-text">
+        <Card title="One Click Tuner" className="center cyan darken-4 white-text">
           <h4><a className="btn" target="_blank" rel="noopener noreferrer" href="https://oneclicktuner.com">Link</a></h4>
+          <a href="https://www.producthunt.com/posts/one-click-tuner?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-one-click-tuner" rel="noopener noreferrer"  target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=153658&theme=light" alt="One Click Tuner - A simple, innovative tuning solution for musicians | Product Hunt Embed" style={{width: 250, height: 54}}/></a>
+          <br /><br />
           <p>Chromatic musical instrument tuner for iOS.</p>
           <br />
           <img src="https://oneclicktuner.com/img/tuner.gif" alt="tuner-gif"
@@ -26,11 +30,21 @@ export default function Products() {
       </Col>
 
       <Col s={12} m={6}>
-        <Card title="statsok.com" className="center indigo darken-4 white-text">
+        <Card title="statsok.com" className="center cyan darken-4 white-text">
           <h4><a className="btn" target="_blank" rel="noopener noreferrer" href="https://statsok.com">Link</a></h4>
           <p>Modern interactive statistics courses.</p>
           <br />
           <img src="https://user-images.githubusercontent.com/8075494/64424484-388d9800-d077-11e9-8652-4ea94df820c9.png" alt="statsok-img" width="100%" style={{borderRadius: 10}}/>
+        </Card>
+      </Col>
+
+      <Col s={12} m={6}>
+        <Card title="OnlineStats.jl" className="center cyan darken-4 white-text">
+          <h4><a className="btn" target="_blank" rel="noopener noreferrer" href="https://github.com/joshday/OnlineStats.jl">Link</a></h4>
+          <p>On-line algorithms for statistics.</p>
+          <br />
+          <img src="https://user-images.githubusercontent.com/8075494/57313750-3d890d80-70be-11e9-99c9-b3fe0de6ea81.png"
+            alt="statsok-img" width="100%" style={{ borderRadius: 10 }} />
         </Card>
       </Col>
     </Row>
--- src/components/Services.js
+++ src/components/Services.js
@@ -5,16 +5,24 @@ export default function Services() {
   return <Container>
     <Row style={{marginTop: 20}}>
       <Col s={12} m={6}>
-        <Card title="Consulting" className="teal darken-4 white-text">
+        <Card title="Consulting" className="black darken-4 white-text">
           <p>We provide a variety of general consulting services: everything from code review to statistical analysis.</p>
+          <br />
+          <Row className="center">
+            <a className="btn btn-large" href="mailto:info@loonanalytics.com?Subject=Consulting">How can we help?</a>
+          </Row>
         </Card>
       </Col>
 
       <Col s={12} m={6}>
-        <Card title="Julia Workshops" className="teal darken-4 white-text">
+        <Card title="Julia Workshops" className="black darken-4 white-text">
           <p>
             Julia workshops are provided in a half-day or full-day format, covering everything from installation and Julia basics to the state-of-the-art of machine learning in Julia.
           </p>
+          <br />
+          <Row className="center">
+            <a className="btn btn-large" href="mailto:info@loonanalytics.com?Subject=Workshops">Contact us for pricing</a>
+          </Row>
         </Card>
       </Col>
 
