f8addb363a57b8ffc11d00e0e56e59d03a93a720
Josh Day <emailjoshday@gmail.com>
2019-09-06 15:45:48 -0400

    wip

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,6 +4,17 @@ import { Row, Col, Card, Container } from 'react-materialize'
 export default function Services() {
   return <Container>
     <Row style={{marginTop: 20}}>
+
+      <Col s={12} m={6}>
+        <Card title="Software" className="black darken-4 white-text">
+          <p>We build custom analytical software when you don't have the resources in-house.</p>
+          <br />
+          <Row className="center">
+            <a className="btn btn-large" href="mailto:info@loonanalytics.com?Subject=Software">What can we build?</a>
+          </Row>
+        </Card>
+      </Col>
+
       <Col s={12} m={6}>
         <Card title="Consulting" className="black darken-4 white-text">
           <p>We provide a variety of general consulting services: everything from code review to statistical analysis.</p>
